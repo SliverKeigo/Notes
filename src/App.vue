1a39d03f13d39024aad832ab1519bb0227f7169a
SliverKeigo <1226355793@qq.com>
2024-02-18 01:49:04 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main>
+        <div class="container">
+          <header>
+            <h1>Notes</h1>
+            <button>+</button>
+          </header>
+           <div class="card-container">
+                <div class="card">
+                  <p class="main-text"></p>
+                  <p class="date"></p>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+
+main{
+  height: 100vh;
+  width: 100vw;
+}
+.container{
+max-width: 1000px;
+padding: 10px;
+margin: 0 auto;
+}
+header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+</style>
